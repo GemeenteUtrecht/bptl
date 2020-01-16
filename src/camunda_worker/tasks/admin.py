d52da520cbfb945e71afa957eb4f145fe72d1614
annashamray <anna.shamray@maykinmedia.nl>
2020-01-16 17:08:20 +0100

    modify TaskMapping admin

--- src/camunda_worker/tasks/admin.py
+++ src/camunda_worker/tasks/admin.py
@@ -3,11 +3,9 @@ from django.contrib import admin
 from .models import TaskMapping
 
 
+@admin.register(TaskMapping)
 class TaskMappingAdmin(admin.ModelAdmin):
     model = TaskMapping
-    # list_display = ("__str__", "active")
-    # list_filter = ("active",)
-    # search_fields = ("topic_name", "callback")
-
-
-admin.site.register(TaskMapping, TaskMappingAdmin)
+    list_display = ("__str__", "active")
+    list_filter = ("active",)
+    search_fields = ("topic_name", "callback")
