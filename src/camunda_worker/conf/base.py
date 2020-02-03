95e2596dd554f8983a15b0ecf139f9d7f64360c6
annashamray <anna.shamray@maykinmedia.nl>
2020-02-03 10:32:47 +0100

    add small comment

--- src/camunda_worker/conf/base.py
+++ src/camunda_worker/conf/base.py
@@ -362,4 +362,4 @@ CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
 # Silenced checks
-SILENCED_SYSTEM_CHECKS = ["utils.E001"]
+SILENCED_SYSTEM_CHECKS = ["utils.E001"]  # for django-celery-beat migrations
