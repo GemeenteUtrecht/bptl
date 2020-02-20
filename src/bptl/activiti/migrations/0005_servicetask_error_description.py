4527d7726bd7cd28399ab82a9983115fa5f89d00
annashamray <anna.shamray@maykinmedia.nl>
2020-02-20 15:38:50 +0100

    fix migrations after rebase

--- src/bptl/activiti/migrations/0005_servicetask_error_description.py
+++ src/bptl/activiti/migrations/0005_servicetask_error_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("activiti", "0003_auto_20200214_1109"),
+        ("activiti", "0004_auto_20200220_1445"),
     ]
 
     operations = [
