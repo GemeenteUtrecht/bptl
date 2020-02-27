af62673445a6b07c3ba064ee8558cb6c26dc7199
Sergei Maertens <sergei@maykinmedia.nl>
2020-02-27 13:11:46 +0100

    :alembic: Reduced POC to processing just the docstring
    String input is now processed into a docutils doctree, which can be
    passed to the specific builders [in theory].

--- lab/main.py
+++ lab/main.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
-
+from docutils.core import publish_doctree
 from sphinx.application import Sphinx
+from sphinx.util import rst
+from sphinx.util.docutils import sphinx_domains
+
+DOCSTRING = """
+A dummy task to demonstrate the registry machinery.
+
+The task receives the :class:`ExternalTask` instance and logs some information,
+after which it completes the task.
+"""
 
 
-def main(builder="html"):
+def main(builder_alias="html"):
     print("Rendering experiments...")
 
-    app = Sphinx(
-        ".",
-        "../doc/",
-        "_build",
-        ".doctrees",
-        builder,
-        confoverrides={"master_doc": "doc",},
-    )
+    app = Sphinx(".", "../doc/", "_build", ".doctrees", builder_alias,)
 
-    app.build(force_all=False, filenames=["doc.rst"])
+    builder = app.builder
+    env = builder.env
+
+    docname = "doc"
+    env.prepare_settings(docname)
+    with sphinx_domains(env), rst.default_role(docname, builder.config.default_role):
+        document = publish_doctree(DOCSTRING, settings_overrides=env.settings)
+
+    print(document)
 
 
 if __name__ == "__main__":
