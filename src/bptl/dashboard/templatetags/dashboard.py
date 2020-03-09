4c7dbc4e1aab6b413f24f634e6c3e333fdfcec34
annashamray <anna.shamray@maykinmedia.nl>
2020-03-09 11:06:55 +0100

    display labels in django-choices

--- src/bptl/dashboard/templatetags/dashboard.py
+++ src/bptl/dashboard/templatetags/dashboard.py
@@ -2,7 +2,8 @@ import json
 
 from django.template import Library
 
-from bptl.tasks.constants import ENGINETYPE_MODEL_MAPPING
+from bptl.tasks.constants import ENGINETYPE_MODEL_MAPPING, EngineTypes
+from bptl.utils.constants import Statuses
 
 register = Library()
 
@@ -11,10 +12,15 @@ register = Library()
 def task_type(task) -> str:
     for type, model in ENGINETYPE_MODEL_MAPPING.items():
         if isinstance(task, model):
-            return type
+            return EngineTypes.values[type]
     return ""
 
 
 @register.filter
 def pretty_json(value: str) -> str:
     return json.dumps(value, indent=4)
+
+
+@register.filter
+def display_status(value: str) -> str:
+    return Statuses.values[value]
