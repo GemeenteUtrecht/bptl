f2c874a55da5764c68b04ee8a6856b8cdc5c5eef
annashamray <anna.shamray@maykinmedia.nl>
2020-03-12 18:07:33 +0100

    add task wrapper

--- src/bptl/work_units/brp/tasks.py
+++ src/bptl/work_units/brp/tasks.py
@@ -3,7 +3,7 @@ from bptl.tasks.registry import register
 
 from .client import get_client_class
 
-__all__ = ["IsAboveAge"]
+__all__ = ["IsAboveAge", "HasDegreeOfKinship"]
 
 
 @register
@@ -37,3 +37,47 @@ class IsAboveAge(WorkUnit):
         is_above_age = None if leeftijd is None else leeftijd >= age
 
         return {"isAboveAge": is_above_age}
+
+
+@register
+class HasDegreeOfKinship(WorkUnit):
+    """
+    Fetches BRP API and return flag if a certain two people have a specified degree of kinship
+
+    Required process variables:
+
+    * ``burgerservicenummer1``: BSN of the first person
+    * ``burgerservicenummer2``: BSN of the second person
+    * ``kinship``: integer, which represents the degree of kinship (blood relations). Values Can be [1..4]
+
+    The task sets the process variables:
+
+    * ``hasDegreeOfKinship``: boolean, which indicate if the requested people have a certain degree of kinship.
+      If the information about relations is not found or if `kinship`` value is not in the required range,
+      ``hasDegreeOfKinship`` will be set as ``none``
+    """
+
+    def perform(self) -> dict:
+        variables = self.task.get_variables()
+        bsn1 = variables["burgerservicenummer1"]
+        bsn2 = variables["burgerservicenummer2"]
+        kinship = variables["kinship"]
+
+        if kinship < 1 or kinship > 4:
+            return {"hasDegreeOfKinship": None}
+
+        client = get_client_class()()
+        client.task = self.task
+        url = f"ingeschrevenpersonen/{bsn1}"
+
+        response = client.get(url)
+        relations = response["_embedded"]
+        parents = relations["ouders"]
+        children = relations["kinderen"]
+
+        if kinship == 1:
+            for rel in parents + children:
+                if rel["burgerservicenummer"] == bsn2:
+                    return {"hasDegreeOfKinship": True}
+
+            return {"hasDegreeOfKinship": False}
