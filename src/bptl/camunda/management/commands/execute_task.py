7ceb6db990fdd68ec99851ac20d6274116defb9e
Sergei Maertens <sergei@maykinmedia.nl>
2020-06-12 11:09:52 +0200

    :sparkles: Added option to force (re) execution of external task
    This makes debugging/development easier with breakpoints without hitting
    expiry timers.

--- src/bptl/camunda/management/commands/execute_task.py
+++ src/bptl/camunda/management/commands/execute_task.py
@@ -1,4 +1,9 @@
+from datetime import timedelta
+
 from django.core.management import BaseCommand, CommandError
+from django.utils import timezone
+
+from bptl.utils.constants import Statuses
 
 from ...models import ExternalTask
 from ...tasks import task_execute_and_complete
@@ -11,12 +16,23 @@ class Command(BaseCommand):
         parser.add_argument(
             "task_id", type=int, help="ID of the External Task in the database"
         )
+        parser.add_argument(
+            "-f",
+            "--force",
+            action="store_true",
+            help="Force execution, even if the task is already executed or failed.",
+        )
 
     def handle(self, **options):
         task_id = options["task_id"]
         task = ExternalTask.objects.filter(id=task_id).first()
         if task is None:
             raise CommandError("Could not find this task.")
+
+        if options["force"]:
+            task.status = Statuses.initial
+            task.lock_expires_at = timezone.now() + timedelta(seconds=10 * 60)
+            task.save()
 
         self.stdout.write("Executing task %s" % task)
         task_execute_and_complete(task_id)
