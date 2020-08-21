1831463bddd226c995fabc68459a581bf458bd89
Sergei Maertens <sergei@maykinmedia.nl>
2020-08-21 16:53:42 +0200

    :white_check_mark: Add callback endpoint auth tests

--- src/bptl/work_units/valid_sign/tests/test_callbacks.py
+++ src/bptl/work_units/valid_sign/tests/test_callbacks.py
@@ -22,3 +22,51 @@ Example callback:
       "createdDate": "2020-08-21T14:12:34.544Z"
     }
 """
+from django.urls import reverse_lazy
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+BODY = {
+    "@class": "com.silanis.esl.packages.event.ESLProcessEvent",
+    "name": "PACKAGE_COMPLETE",
+    "sessionUser": "30ba8506-9819-46aa-a22d-c0114ba34cd0",
+    "packageId": "LWsUTvGgE4WpOvaQPT16idnxNj8=",
+    "message": None,
+    "documentId": None,
+    "createdDate": "2020-08-21T14:12:34.544Z",
+}
+
+
+class CallbackTests(APITestCase):
+
+    endpoint = reverse_lazy("valid_sign:callbacks")
+
+    def test_no_auth_perm_denied(self):
+        response = self.client.post(self.endpoint, BODY)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_invalid_header_pattern(self):
+        values = ["Token foo", "Basic foo bar baz", "some-key", "Basic"]
+        for value in values:
+            with self.subTest(header_value=value):
+                response = self.client.post(
+                    self.endpoint, BODY, HTTP_AUTHORIZATION=value
+                )
+
+                self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_wrong_basic_auth_key(self):
+        response = self.client.post(
+            self.endpoint, BODY, HTTP_AUTHORIZATION="Basic wrong-key"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_correct_basic_auth_key(self):
+        response = self.client.post(
+            self.endpoint, BODY, HTTP_AUTHORIZATION="Basic some-random-key"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
