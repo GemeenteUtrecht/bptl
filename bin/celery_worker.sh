6c09b2169f216c4f75964ee3d304bd5a5d108c08
Daniel Ammeraal <danielammeraal@gmail.com>
2020-09-21 11:39:23 +0200

    Changed celery worker to accept positional arguments: queue, worker_name and pod_name

--- bin/celery_worker.sh
+++ bin/celery_worker.sh
@@ -5,9 +5,17 @@ set -e
 LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
 CONCURRENCY=${CELERY_WORKER_CONCURRENCY:-1}
 
-echo "Starting celery worker"
+QUEUE=${1:-${CELERY_WORKER_QUEUE:=celery}}
+WORKER_NAME=${2:-${CELERY_WORKER_NAME:=$QUEUE}}
+POD_NAME=${3:-${hostname:=default}}
+
+NAME="${WORKER_NAME}.${POD_NAME}"
+
+echo "Starting celery worker $NAME with queue $QUEUE"
 celery worker \
     --app bptl \
+    -Q $QUEUE \
+    -n $NAME \
     -l $LOGLEVEL \
     --workdir src \
     -O fair \
