c43fc52c73d632d26382ec6454fd97cf2cd3dc5e
Sergei Maertens <sergei@maykinmedia.nl>
2020-10-21 17:43:11 +0200

    :sparkles: Added work unit to look up a zaak reference

--- src/bptl/work_units/zgw/tasks/__init__.py
+++ src/bptl/work_units/zgw/tasks/__init__.py
@@ -2,7 +2,7 @@ from .documents import LockDocument, UnlockDocument  # noqa
 from .resultaat import CreateResultaatTask  # noqa
 from .rol import CreateRolTask  # noqa
 from .status import CreateStatusTask  # noqa
-from .zaak import CloseZaakTask, CreateZaakTask  # noqa
+from .zaak import CloseZaakTask, CreateZaakTask, LookupZaak  # noqa
 from .zaak_relations import (  # noqa
     CreateEigenschap,
     CreateZaakObject,
