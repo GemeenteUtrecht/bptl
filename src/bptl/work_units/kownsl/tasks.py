e901d41a44f2f4ceb33eee512b84ba3bfaa6c948
Sergei Maertens <sergei@maykinmedia.nl>
2020-11-11 10:07:35 +0100

    :fire: Remove deprecated Kownsl task
    This task was deprecated a couple of weeks ago. No BPMN models for
    Utrecht currently use this task, so it was safe to delete.

--- src/bptl/work_units/kownsl/tasks.py
+++ src/bptl/work_units/kownsl/tasks.py
@@ -44,48 +44,6 @@ def get_review_request(task: BaseTask) -> dict:
 
 
 @register
-def finalize_review_request(task: BaseTask) -> dict:
-    """
-    Update a review request in Kownsl.
-
-    DEPRECATED - do not use anymore.
-
-    Review requests can be requests for advice or approval. This tasks registers the
-    case used for the actual review with the review request, and derives the frontend
-    URL for end-users where they can submit their review.
-
-    In the task binding, the service with alias ``kownsl`` must be connected, so that
-    this task knows which endpoints to contact.
-
-    **Required process variables**
-
-    * ``reviewRequestId``: the identifier of the Kownsl review request.
-    * ``zaakUrl``: URL reference to the zaak used for the review itself.
-
-    **Sets the process variables**
-
-    * ``doReviewUrl``: the frontend URL that reviewers visit to submit the review
-
-    """
-    variables = task.get_variables()
-
-    request_id = check_variable(variables, "reviewRequestId")
-    zaak_url = check_variable(variables, "zaakUrl")
-
-    client = get_client(task)
-
-    resp_data = client.partial_update(
-        "reviewrequest",
-        data={"review_zaak": zaak_url},
-        uuid=request_id,
-    )
-
-    return {
-        "doReviewUrl": resp_data["frontend_url"],
-    }
-
-
-@register
 def get_approval_status(task: BaseTask) -> dict:
     """
     Get the result of an approval review request.
