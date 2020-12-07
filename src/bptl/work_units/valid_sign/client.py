5c80062ab52a4850ba44d747fcc3a455d233d3fd
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-07 19:19:25 +0100

    :art: Some more de-duplication of code in ValidSign client

--- src/bptl/work_units/valid_sign/client.py
+++ src/bptl/work_units/valid_sign/client.py
@@ -10,11 +10,11 @@ from zgw_consumers.models import Service
 from bptl.credentials.api import get_credentials
 from bptl.tasks.models import BaseTask, DefaultService
 from bptl.tasks.registry import register
-from bptl.work_units.zgw.tasks.base import NoService
+
+from ..clients import APP_ID_PROCESS_VAR_NAME
+from ..services import get_alias_service
 
 ALIAS = "ValidSignAPI"
-
-PROCESS_VAR_NAME = "bptlAppId"
 
 require_validsign_service = register.require_service(
     APITypes.orc,
@@ -24,22 +24,13 @@ require_validsign_service = register.require_service(
 
 
 def get_client(task: BaseTask) -> ZGWClient:
-    # get the service and credentials
-    try:
-        default_service = DefaultService.objects.filter(
-            task_mapping__topic_name=task.topic_name,
-            alias=ALIAS,
-        ).get()
-    except DefaultService.DoesNotExist:
-        raise NoService(f"No '{ALIAS}' service configured.")
-
-    service = default_service.service
+    service = get_alias_service(task, ALIAS)
 
     client = service.build_client()
     client._log.task = task
 
     # set the auth if we have the bptlAppId set
-    app_id = task.get_variables().get(PROCESS_VAR_NAME)
+    app_id = task.get_variables().get(APP_ID_PROCESS_VAR_NAME)
     if app_id:
         auth_headers = get_credentials(app_id, service)[service]
         if auth_headers:
@@ -51,7 +42,7 @@ def get_client(task: BaseTask) -> ZGWClient:
 
 class DRCClientPool:
     def __init__(self, task_variables: dict):
-        self.app_id = task_variables.get(PROCESS_VAR_NAME)
+        self.app_id = task_variables.get(APP_ID_PROCESS_VAR_NAME)
         self._services = task_variables.get("services")
 
         self._clients = []  # mapping of API root to service, works as a cache
