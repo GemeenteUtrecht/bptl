a6223d185bc25f79d0a00f5769c796b529ef7668
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-07 17:20:06 +0100

    :children_crossing: Manage app credentials inline

--- src/bptl/credentials/admin.py
+++ src/bptl/credentials/admin.py
@@ -4,10 +4,18 @@ from django.utils.translation import gettext_lazy as _
 from .models import App, AppServiceCredentials
 
 
+class AppServiceCredentialsInline(admin.TabularInline):
+    model = AppServiceCredentials
+    fields = ("service", "client_id", "secret", "header_key", "header_value")
+    autocomplete_fields = ("service",)
+    extra = 1
+
+
 @admin.register(App)
 class AppAdmin(admin.ModelAdmin):
     list_display = ("label", "app_id")
     search_fields = ("app_id", "label")
+    inlines = [AppServiceCredentialsInline]
 
 
 @admin.register(AppServiceCredentials)
