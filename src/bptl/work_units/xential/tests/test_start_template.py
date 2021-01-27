edb64c910ff926e4745ea77dbf3bb69355d3c74d
silvia <silvia@maykinmedia.nl>
2021-01-27 16:32:37 +0100

    :white_check_mark: [#110] Updated existing test

--- src/bptl/work_units/xential/tests/test_start_template.py
+++ src/bptl/work_units/xential/tests/test_start_template.py
@@ -6,14 +6,14 @@ from zgw_consumers.constants import APITypes, AuthTypes
 from zgw_consumers.models import Service
 
 from bptl.camunda.models import ExternalTask
-from bptl.credentials.tests.factories import AppServiceCredentialsFactory
 from bptl.tasks.models import TaskMapping
 from bptl.tasks.tests.factories import DefaultServiceFactory
 
+from ..models import XentialTicket
 from ..tasks import start_xential_template
 
-XENTIAL_API_ROOT = "https://alfresco.utrechtproeftuin.nl/alfresco/s/"
-TEMPLATE_URL = "https://xential.nl/xential?resumeApplication=ID&loginTicketUuid=Uuid&afterBuildAction=close"
+XENTIAL_API_ROOT = "https://xentiallabs.com/xential/modpages/next.oas/api/"
+DRC_ROOT = "https://openzaak.nl/documenten/api/v1/"
 
 
 @requests_mock.Mocker()
@@ -33,38 +33,50 @@ class XentialTaskTests(TestCase):
             auth_type=AuthTypes.api_key,
             oas="",
         )
-
         DefaultServiceFactory.create(
             task_mapping=mapping,
             service=xential,
             alias="xential",
         )
-        AppServiceCredentialsFactory.create(
-            app__app_id="some-app-id",
-            service=xential,
-            header_key="Authorization",
-            header_value="Basic ZGVtbzpkZW1v",
+
+        drc = Service.objects.create(
+            label="Documenten API",
+            api_type=APITypes.drc,
+            api_root=DRC_ROOT,
+            auth_type=AuthTypes.api_key,
+            oas="",
+        )
+        DefaultServiceFactory.create(
+            task_mapping=mapping,
+            service=drc,
+            alias="drc",
         )
 
-    def test_start_template(self, m):
-        m.get(
-            f"{XENTIAL_API_ROOT}xential/templates",
+    def test_start_silent_template(self, m):
+        m.post(
+            f"{XENTIAL_API_ROOT}auth/whoami",
             json={
-                "data": {
-                    "templates": [
-                        {"templateName": "Interactief", "templateUuid": "1234567890"},
-                        {"templateName": "Silent", "templateUuid": "0987654321"},
-                    ],
-                    "params": {"sessionId": "12345678901234", "uuid": "09876543211234"},
-                }
+                "user": {
+                    "uuid": "a4664ccb-259e-4107-b800-d8e5a764b9dd",
+                    "userName": "testuser",
+                },
+                "XSessionId": "f7f588eb-b7c9-4d23-babd-4a98a9326367",
             },
         )
         m.post(
-            f"{XENTIAL_API_ROOT}xential/templates/start?nodeRef=some/node/ref",
+            f"{XENTIAL_API_ROOT}createTicket",
+            json={"ticketId": "b0fdd542-0cc4-44a1-8dfb-808436123ce8"},
+        )
+        m.post(
+            f"{XENTIAL_API_ROOT}document/startDocument?ticketUuid=b0fdd542-0cc4-44a1-8dfb-808436123ce8",
             json={
-                "xentialTemplateUrl": TEMPLATE_URL,
-                "nodeRefParentFolder": "123456789098765",
+                "documentUuid": "527f6ea1-e292-41ec-ac5b-8f8feddf153f",
+                "resumeUrl": "/xential?resumeApplication=527f6ea1-e292-41ec-ac5b-8f8feddf153f&loginTicketUuid=4c68ae87-1e13-49db-8744-600a4c86a067",
             },
+        )
+        m.post(
+            f"{XENTIAL_API_ROOT}document/buildDocument?documentUuid=527f6ea1-e292-41ec-ac5b-8f8feddf153f",
+            json={"success": True},
         )
 
         external_task = ExternalTask.objects.create(
@@ -73,17 +85,86 @@ class XentialTaskTests(TestCase):
             task_id="test-task-id",
             variables={
                 "bptlAppId": serialize_variable("some-app-id"),
-                "nodeRef": serialize_variable("some/node/ref"),
                 "templateUuid": serialize_variable(
-                    "540d57cb-c7f2-4423-821e-fc17c857a404"
+                    "3e09b238-0617-47c1-8e6a-f6227b3d542e"
                 ),
-                "filename": serialize_variable("some-file-name"),
+                "interactive": serialize_variable("False"),
                 "templateVariables": serialize_variable(
-                    {"variable1": "String", "variable2": "String"}
+                    {"textq1": "Answer1", "dateq1": "31-12-20"}
+                ),
+                "documentMetadata": serialize_variable(
+                    {
+                        "bronorganisatie": "517439943",
+                        "creatiedatum": "01-01-2021",
+                        "titel": "Test Document",
+                        "auteur": "Test Author",
+                        "taal": "eng",
+                        "informatieobjecttype": "http://openzaak.nl/catalogi/api/v1/informatieobjecttypen/06d3a135-bc20-4fce-9add-f69d8e585917",
+                    }
                 ),
             },
         )
 
         result = start_xential_template(external_task)
 
-        self.assertEqual(result, {"buildId": None, "xentialTemplateUrl": TEMPLATE_URL})
+        self.assertEqual(result, {"bptlDocumentUrl": ""})
+
+        # Check that the XentialTicket object was created
+        xential_ticket = XentialTicket.objects.get()
+
+        self.assertEqual(
+            "b0fdd542-0cc4-44a1-8dfb-808436123ce8", str(xential_ticket.ticket_uuid)
+        )
+
+    def test_start_interactive_template(self, m):
+        m.post(
+            f"{XENTIAL_API_ROOT}auth/whoami",
+            json={
+                "user": {
+                    "uuid": "a4664ccb-259e-4107-b800-d8e5a764b9dd",
+                    "userName": "testuser",
+                },
+                "XSessionId": "f7f588eb-b7c9-4d23-babd-4a98a9326367",
+            },
+        )
+        m.post(
+            f"{XENTIAL_API_ROOT}createTicket",
+            json={"ticketId": "b0fdd542-0cc4-44a1-8dfb-808436123ce8"},
+        )
+
+        external_task = ExternalTask.objects.create(
+            topic_name="xential-topic",
+            worker_id="test-worker-id",
+            task_id="test-task-id",
+            variables={
+                "bptlAppId": serialize_variable("some-app-id"),
+                "templateUuid": serialize_variable(
+                    "3e09b238-0617-47c1-8e6a-f6227b3d542e"
+                ),
+                "interactive": serialize_variable("True"),
+                "templateVariables": serialize_variable(
+                    {"textq1": "Answer1", "dateq1": "31-12-20"}
+                ),
+                "documentMetadata": serialize_variable(
+                    {
+                        "bronorganisatie": "517439943",
+                        "creatiedatum": "01-01-2021",
+                        "titel": "Test Document",
+                        "auteur": "Test Author",
+                        "taal": "eng",
+                        "informatieobjecttype": "http://openzaak.nl/catalogi/api/v1/informatieobjecttypen/06d3a135-bc20-4fce-9add-f69d8e585917",
+                    }
+                ),
+            },
+        )
+
+        result = start_xential_template(external_task)
+
+        # Check that the XentialTicket object was created
+        xential_ticket = XentialTicket.objects.get()
+        bptl_ticket_url = f"https://example.com/contrib/api/xential/interactive_document/{xential_ticket.bptl_ticket_uuid}"
+
+        self.assertEqual(
+            "b0fdd542-0cc4-44a1-8dfb-808436123ce8", str(xential_ticket.ticket_uuid)
+        )
+        self.assertEqual(bptl_ticket_url, result["bptlDocumentUrl"])
