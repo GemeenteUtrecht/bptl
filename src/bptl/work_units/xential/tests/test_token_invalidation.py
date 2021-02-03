68b701c57711533ff1ad5e9461aa1a36f3a44828
silvia <silvia@maykinmedia.nl>
2021-02-03 17:19:01 +0100

    :white_check_mark: [#117] Added token tests

---
+++ src/bptl/work_units/xential/tests/test_token_invalidation.py
@@ -0,0 +1,67 @@
+from datetime import timedelta
+
+from django.test import TestCase, override_settings
+
+from django_camunda.camunda_models import Task, factory
+from django_camunda.utils import serialize_variable, underscoreize
+from freezegun import freeze_time
+
+from bptl.camunda.models import ExternalTask
+from bptl.camunda.tests.factories import ExternalTaskFactory
+
+from ..models import XentialTicket
+from ..tokens import token_generator
+
+
+@override_settings(EXECUTE_TASK_TOKEN_TIMEOUT_DAYS=7)
+class TokenInvalidationTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.task = ExternalTaskFactory.create(instance_id="some-instance-id")
+        cls.ticket = XentialTicket.objects.create(
+            task=cls.task,
+            bptl_ticket_uuid="2d30f19b-8666-4f45-a8da-78ad7ed0ef4d",
+            ticket_uuid="f15aceb4-b316-45bc-8353-7906ae125557",
+            is_ticket_complete=False,
+        )
+
+    def test_valid_token(self):
+        token = token_generator.make_token(self.ticket)
+
+        for day in range(8):
+            with self.subTest(day_offset=day):
+                with freeze_time(timedelta(days=day)):
+                    valid = token_generator.check_token(self.ticket, token)
+
+                self.assertTrue(valid)
+
+    def test_token_expired(self):
+        token = token_generator.make_token(self.ticket)
+
+        with freeze_time(timedelta(days=8)):
+            valid = token_generator.check_token(self.ticket, token)
+
+        self.assertFalse(valid)
+
+    def test_token_invalidated_properties_changed(self):
+        token = token_generator.make_token(self.ticket)
+
+        self.ticket.is_ticket_complete = True
+        self.ticket.save()
+
+        valid = token_generator.check_token(self.ticket, token)
+
+        self.assertFalse(valid)
+
+    def test_wrong_format_token(self):
+
+        valid = token_generator.check_token(self.ticket, "dummy")
+
+        self.assertFalse(valid)
+
+    def test_invalid_timestamp_b36(self):
+        valid = token_generator.check_token(self.ticket, "$$$-blegh")
+
+        self.assertFalse(valid)
