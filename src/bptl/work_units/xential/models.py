973783ccc3d05053c6a99a430f3be712549ce7a1
silvia <silvia@maykinmedia.nl>
2021-02-05 15:31:02 +0100

    :sparkles: [#117] Xential configuration model

--- src/bptl/work_units/xential/models.py
+++ src/bptl/work_units/xential/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.crypto import get_random_string
+from django.utils.encoding import force_text
 from django.utils.translation import gettext_lazy as _
+
+from solo.models import SingletonModel
 
 
 class XentialTicket(models.Model):
@@ -22,3 +27,29 @@ class XentialTicket(models.Model):
 
     def __str__(self):
         return str(self.bptl_ticket_uuid)
+
+
+def get_auth_key():
+    return get_random_string(length=50)
+
+
+class XentialConfiguration(SingletonModel):
+    auth_key = models.CharField(
+        _("authentication key"),
+        max_length=200,
+        default=get_auth_key,
+        help_text=_(
+            "The unique authentication key for Xential API to authenticate itself when"
+            "returning a newly built document."
+        ),
+    )
+
+    class Meta:
+        verbose_name = _("Xential Configuration")
+
+    def __str__(self):
+        return force_text(self._meta.verbose_name)
+
+    @property
+    def callback_url(self):
+        return reverse("Xential:xential-callbacks")
