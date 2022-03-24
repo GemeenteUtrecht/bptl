f7b8cc53d4d94d59d1ed966755900b4efcccd3b4
damm89 <danielammeraal@gmail.com>
2022-03-24 17:26:57 +0100

    :bookmark: Bump to 0.1.5

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ copyright = "2020, Maykin Media"
 author = "Maykin Media"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.4"
+release = "0.1.5"
 
 
 # -- General configuration ---------------------------------------------------
