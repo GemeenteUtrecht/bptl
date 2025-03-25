e27a1bccff9863417adf5bc2cfc46a7d6f26586a
damm89 <danielammeraal@gmail.com>
2025-03-25 12:35:13 +0100

    black

--- src/bptl/openklant/migrations/0003_rename_task_uuid_openklantinternaltaskmodel_task_id.py
+++ src/bptl/openklant/migrations/0003_rename_task_uuid_openklantinternaltaskmodel_task_id.py
@@ -6,13 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('openklant', '0002_auto_20250324_1236'),
+        ("openklant", "0002_auto_20250324_1236"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='openklantinternaltaskmodel',
-            old_name='task_uuid',
-            new_name='task_id',
+            model_name="openklantinternaltaskmodel",
+            old_name="task_uuid",
+            new_name="task_id",
         ),
     ]
